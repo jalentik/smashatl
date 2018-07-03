e622c1aad5c93f132c3dde3d5fd5e6eed44d0ea8
jalentik <jalen@myibistro.com>
2018-07-02 20:14:03 -0400

    Added Events page, needs react-sticky implementation or invisible scroll container

--- src/App.js
+++ src/App.js
@@ -6,15 +6,12 @@ import logo3 from './logo2.gif';
 import fb from './fb.png';
 import twit from './twit.png';
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-
 import {
-  Router,
   Route,
   NavLink,
-  HashRouter,
   Switch,
-  Redirect,
-  BrowserRouter
+  BrowserRouter,
+  withRouter
 } from "react-router-dom";
 import home from './home';
 import events from './events';
@@ -22,81 +19,79 @@ import smashmap from './smashmap';
 import account from './account';
 import faq from './faq';
 
-const App = () => (
 
+const App = () => (
   <BrowserRouter>
     <Route
       render={({ location }) => (
-        
-        <div className="app">
-          
-          <div className="header">
-            <div className="header-left">
-              <div className="nav-container">
+        <div className="app" >
+                  <div className="header" ref='header'>
+                    <div className="header-left">
+                      <div className="nav-container">
 
-                <label className="nav-item">
-                  <NavLink exact to="/">Home</NavLink>
-                </label>
-                <div className="nav-item">
-                  <NavLink to="/events">Events</NavLink>
+                        <label className="nav-item">
+                          <NavLink exact to="/">Home</NavLink>
+                        </label>
+                        <div className="nav-item">
+                          <NavLink to="/events">Events</NavLink>
 
-                </div>
-                <div className="nav-item">
-                  <NavLink to="/smashmap">Smash Map</NavLink>
-                </div>
+                        </div>
+                        <div className="nav-item">
+                          <NavLink to="/smashmap">Smash Map</NavLink>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="header-middle">
+
+                      <img src={logo3} className="logo" />
+                    </div>
+                    <div className="header-right">
+
+                      <div className="nav-container">
+
+                        <div className="nav-item">
+                          <NavLink to="/faq"> FAQ</NavLink>
+                        </div>
+                        <div className="nav-item">
+                          <NavLink to="/account">Account</NavLink>
+                        </div>
+
+                      </div>
+                      <div className="contact">
+                        <p>telephone: (770)111-1111</p>
+                        <a href="">email: team@smashatl.com</a>
+                        <div style={{ display: 'flex', flexDirection: 'row', maxHeight: '50px', paddingTop: '10px' }}>
+                          <img src={fb} className='social-image' />
+                          <img src={twit} className='social-image' />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+            <div className="content">
+              <div className="content-side"></div>
+              <div className="content-main" >
+                <TransitionGroup>
+                  <CSSTransition in key={location.key} classNames="page-transition" timeout={500}>
+                    <Switch location={location}>
+
+                      <Route exact path="/" component={home} />
+                      <Route exact path="/events" component={events} />
+                      <Route exact path="/smashmap" component={smashmap} />
+                      <Route exact path="/faq" component={faq} />
+                      <Route exact path="/account" component={account} />
+
+                      <Route render={() => <div>Not Found</div>} />
+
+                    </Switch>
+                  </CSSTransition>
+                </TransitionGroup>
               </div>
+              <div className="content-side"></div>
+
             </div>
-            <div className="header-middle">
-
-              <img src={logo3} className="logo" />
-            </div>
-            <div className="header-right">
-
-              <div className="nav-container">
-
-                <div className="nav-item">
-                  <NavLink to="/faq"> FAQ</NavLink>
-                </div>
-                <div className="nav-item">
-                  <NavLink to="/account">Account</NavLink>
-                </div>
-
-              </div>
-              <div className="contact">
-                <p>telephone: (770)111-1111</p>
-                <a href="">email: team@smashatl.com</a>
-                <div style={{ display: 'flex', flexDirection: 'row', maxHeight: '50px', paddingTop: '10px' }}>
-                  <img src={fb} className='social-image' />
-                  <img src={twit} className='social-image' />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="content">
-            <div className="content-side"></div>
-            <div className="content-main" >
-              <TransitionGroup>
-                <CSSTransition key={location.key} classNames="fade" timeout={500}>
-                  <Switch location={location}>
-
-                    <Route exact path="/" component={home} />
-                    <Route exact path="/events" component={events} />
-                    <Route exact path="/smashmap" component={smashmap} />
-                    <Route exact path="/faq" component={faq} />
-                    <Route exact path="/account" component={account} />
-
-                    <Route render={() => <div>Not Found</div>} />
-
-                  </Switch>
-                </CSSTransition>
-              </TransitionGroup>
-            </div>
-            <div className="content-side"></div>
-
-          </div>
-
         </div>
+
       )}
     />
   </BrowserRouter>
--- src/events.js
+++ src/events.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import typo from './typohouse.png';
+import nvm from './nevermore.jpg';
+
+function EventsList(props) {
+    const items = props.events;
+    const listItems = items.map((item) =>
+        <a href={item.link}>
+            <li className="events-list-item">
+                <img src={require('' +item.img)} alt='' />
+                <h1>{item.title}</h1>
+                <h2>{item.alt}</h2>
+            </li>
+        </a>
+    );
+    return (
+
+        <ul className="events-list">
+            {listItems}
+        </ul>
+
+    )
+
+}
+const datasource = [
+    { img: './typohouse.png', title: "Typo House Macon", alt: "Hosted by Typo House Games", date: "July 3rd", location: "4437 Columbus Rd Suit 102, Macon, GA 31206", link: "https://www.facebook.com/events/231830774068344/" },
+    { img: './nevermore.jpg', title: "Nevermore", alt: "$50 Pot Bonus - Hosted by Justin Harvey", date: "July 14th", location: "688 Whitlock Ave NW, Marietta, GA 30064-3152, United States", link: "https://www.facebook.com/events/1662840307088196/" },
+    { img: './ssmash.jpg', title: "Summer Smash at Cardboard Castle", alt: "($50 Pot Bonus) - Hosted by Rafael Cumbermack", date: "July 18th", location: "4015 Columbia Rd, Augusta, Georgia 30907", link: "https://www.facebook.com/events/385766768612458/" },
+    { img: './gb.jpg', title: "Gwinnett Brawl", alt: "Hosted by GB League", date: "July 28th", location: "3700 Satellite Blvd, Ste 7B, Duluth, Georgia 30096", link: "https://www.facebook.com/events/1738378982919039/" }
+]
+
 class event extends Component {
     render() {
         return (
-            <div >
-            TODO
-            </div>        )
+            <div className="events-content">
+                <EventsList events={datasource} />
+            </div>
+        )
     }
 }
 
